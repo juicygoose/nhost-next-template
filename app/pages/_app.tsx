74d879785b202844ede35c0c924c7f5f4899b194
Alex <alex.jousse@icloud.com>
2022-12-08 11:02:18 +0100

    Add nhost configuration to next project

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,13 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { NhostNextProvider } from '@nhost/nextjs'
+import { nhost } from '../lib/nhost'
 import { Toaster } from 'react-hot-toast'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NhostNextProvider nhost={nhost} initial={pageProps.nhostSession}>
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </NhostNextProvider>
   )
 }
 
